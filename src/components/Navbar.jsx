e8b989c2121c2bd151dcf9e3ff1a906d3ae2c9ef
PsychonautHashish <mbuthiaeddy@gmail.com>
2025-03-08 12:52:01 +0300

    Added an Svg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="nav-content">
-        <h1 className="logo">FireSafe Equipment</h1>
+        <h1 className="logo">FireGuard Equipment</h1>
         <div className="nav-links">
           <Link to="/" className="nav-link">Home</Link>
           <Link to="/products" className="nav-link">Products</Link>
